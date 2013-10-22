21c1a2973b3a60855afb534a8834fad92e782ded
Mohammed Sadiq <sadiqmmm@gmail.com>
2013-10-22 18:15:15 +0530

    Add jquery video show / hide

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+$(document).ready( function() {
+    // Value when you load the page for the first time
+    // Will return 0 the first time it's called
+    var radio_button_value = getRadioValue();
+
+    $('input[name=jq1]:radio').click( function() {
+        // Will get the newly selected value
+        radio_button_value = getRadioValue();
+        console.log(radio_button_value);
+
+        if(radio_button_value == 135) {
+			 
+			$('.right-ans').remove();
+			$('.wrong-ans').remove();
+			$('.show').remove();
+			$('#result1').append( "<p class='right-ans alert alert-success'><strong>Correct Answer &nbsp; &nbsp;</strong><div class='show'>Click to (Show / Hide) Video</div></p>" );
+		}
+		else{
+			 
+			$('.right-ans').remove();
+			$('.wrong-ans').remove();
+			$('.show').remove();
+			$('#result1').append( "<p class='wrong-ans alert alert-danger'><strong>Wrong Answer &nbsp; &nbsp;</strong> <div class='show'>Click to (Show / Hide) Video</div></p>" );
+		}
+
+    });
+	$("#see-video").hide(); 
+	
+		 
+		$('#result1').on('click', '.show', function() {
+			$("#see-video").slideToggle('slow',function(){});
+		});
+
+    function getRadioValue () {
+	    if( $('input[name=jq1]:radio:checked').length > 0 ) {
+	        return $('input[name=jq1]:radio:checked').val();
+	    }
+	    else {
+	        return 0;
+	    }
+	}
+
+
+});
