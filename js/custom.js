c9c3e886848369ec6555cbdb9a76f2c0079269f9
Mohammed Sadiq <sadiqmmm@gmail.com>
2013-12-11 16:59:45 +0530

    Add Quiz 1 bit 2

--- js/custom.js
+++ js/custom.js
@@ -1,42 +1,87 @@
 $(document).ready( function() {
     // Value when you load the page for the first time
     // Will return 0 the first time it's called
-    var radio_button_value = getRadioValue();
+    var radio_button_value_jq1 = getRadioValuejq1();
+    var radio_button_value_jq2 = getRadioValuejq2();
 
-    $('input[name=jq1]:radio').click( function() {
-        // Will get the newly selected value
-        radio_button_value = getRadioValue();
-        console.log(radio_button_value);
+    
 
-        if(radio_button_value == 135) {
+
+    $('#quiz-case-study-one').submit( function( event ) {
+         
+        event.preventDefault(); // Prevent from default submit button behaviour
+
+ 		// Will get the newly selected value
+        radio_button_value_jq1 = getRadioValuejq1();    
+        radio_button_value_jq2 = getRadioValuejq2();       
+
+        // jq1
+        if(radio_button_value_jq1 == 135) {
 			 
-			$('.right-ans').remove();
-			$('.wrong-ans').remove();
-			$('.show').remove();
-			$('#result1').append( "<p class='right-ans alert alert-success'><strong>Correct Answer &nbsp; &nbsp;</strong><span class='show'>Click to (Show / Hide) Video</span></p>" );
+			$('#result-jq1 .right-ans').remove();
+			$('#result-jq1 .wrong-ans').remove();
+			$('#result-jq1 .show').remove();
+			$('#result-jq1').append( "<p class='right-ans alert alert-success'><strong>(1) Correct Answer &nbsp; &nbsp;</strong><span class='show'>Click to (Show / Hide) Video</span></p>" );
 		}
 		else{
 			 
-			$('.right-ans').remove();
-			$('.wrong-ans').remove();
-			$('.show').remove();
-			$('#result1').append( "<p class='wrong-ans alert alert-danger'><strong>Wrong Answer &nbsp; &nbsp;</strong> <span class='show'>Click to (Show / Hide) Video</span></p>" );
+			$('#result-jq1 .right-ans').remove();
+			$('#result-jq1 .wrong-ans').remove();
+			$('#result-jq1 .show').remove();
+			$('#result-jq1').append( "<p class='wrong-ans alert alert-danger'><strong>(1) Wrong Answer &nbsp; &nbsp;</strong> <span class='show'>Click to (Show / Hide) Video</span></p>" );
 		}
 
+		    // jq2
+        if(radio_button_value_jq2 == "absent") {
+			 
+			$('#result-jq2 .right-ans').remove();
+			$('#result-jq2 .wrong-ans').remove();
+			$('#result-jq2 .show').remove();
+			$('#result-jq2').append( "<p class='right-ans alert alert-success'><strong>(2) Correct Answer &nbsp; &nbsp;</strong><span class='show'>Click to (Show / Hide) Video</span></p>" );
+		}
+		else{
+			 
+			$('#result-jq2 .right-ans').remove();
+			$('#result-jq2 .wrong-ans').remove();
+			$('#result-jq2 .show').remove();
+			$('#result-jq2').append( "<p class='wrong-ans alert alert-danger'><strong>(2) Wrong Answer &nbsp; &nbsp;</strong> <span class='show'>Click to (Show / Hide) Video</span></p>" );
+		}
+
+ 
+
     });
-	$("#see-video").hide(); 
+
+
+
+	$("#see-video-jq1").hide(); 
+	$("#see-video-jq2").hide(); 
 	
 		 
-		$('#result1').on('click', '.show', function() {
-			$("#see-video").slideToggle('slow',function(){
-					$("html, body").animate({ scrollTop: $('#result1').offset().top -60}, 600);
+		$('#result-jq1').on('click', '.show', function() {
+			$("#see-video-jq1").slideToggle('slow',function(){
+					$("html, body").animate({ scrollTop: $('#result-jq1').offset().top -60}, 600);
        				 return false;
 			});
 		});
 
-    function getRadioValue () {
+		$('#result-jq2').on('click', '.show', function() {
+			$("#see-video-jq2").slideToggle('slow',function(){
+					$("html, body").animate({ scrollTop: $('#result-jq2').offset().top -60}, 600);
+       				 return false;
+			});
+		});
+
+    function getRadioValuejq1 () {
 	    if( $('input[name=jq1]:radio:checked').length > 0 ) {
 	        return $('input[name=jq1]:radio:checked').val();
+	    }
+	    else {
+	        return 0;
+	    }
+	}
+	function getRadioValuejq2 () {
+	    if( $('input[name=jq2]:radio:checked').length > 0 ) {
+	        return $('input[name=jq2]:radio:checked').val();
 	    }
 	    else {
 	        return 0;
